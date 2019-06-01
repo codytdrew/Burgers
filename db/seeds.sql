fc61664eed77f843449a191a5f258eb0727dc196
John Florence <codytdrew@gmail.com>
2019-05-31 22:12:10 -0700

    MySQL Setup

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burger (burger_name, devoured) VALUES ('Guac Burger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Western BBQ Burger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Chilli Burger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Hawaiian Burger', false);
